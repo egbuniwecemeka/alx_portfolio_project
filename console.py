8def475fa7b912bc960768928e5bb218d9ea1276
egbuniwecemeka <egbuniwecemeka.com>
2024-12-22 19:25:29 +0100

    Console 0.0.1

--- console.py
+++ console.py
@@ -2,12 +2,19 @@ import cmd
 
 class HBNBCommand(cmd.Cmd):
     """Command interpreter class"""
+
+    prompt = '(hbnb)'
+    
     def do_greet(self, user):
         """greet the user"""
         if user:
             print(f'hi, {user}')
         else:
             print('hi')
+
+    def emptyline(self):
+        """Does nothing on empty input"""
+        pass
     
     def do_EOF(self, line):
         return True
