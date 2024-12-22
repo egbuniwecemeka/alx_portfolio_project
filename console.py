238e0fccb4a0e05844f4ada135a94e2f3a6d33e5
egbuniwecemeka <egbuniwecemeka.com>
2024-12-22 19:07:14 +0100

    Console 0.0.1

--- console.py
+++ console.py
@@ -2,8 +2,12 @@ import cmd
 
 class HBNBCommand(cmd.Cmd):
     """Command interpreter class"""
-    def do_greet(self, line):
-        print('Hello')
+    def do_greet(self, user):
+        """greet the user"""
+        if user:
+            print(f'hi, {user}')
+        else:
+            print('hi')
     
     def do_EOF(self, line):
         return True
