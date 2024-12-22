990f6d8e2714c0226275844078002ef9a3c8b6eb
egbuniwecemeka <egbuniwecemeka.com>
2024-12-22 19:35:38 +0100

    Console 0.0.1

--- console.py
+++ console.py
@@ -4,20 +4,27 @@ class HBNBCommand(cmd.Cmd):
     """Command interpreter class"""
 
     prompt = '(hbnb)'
-    
-    def do_greet(self, user):
-        """greet the user"""
-        if user:
-            print(f'hi, {user}')
-        else:
-            print('hi')
+
+    def do_quit(self, line):
+        """Exits the program"""
+        return True
+
+    def do_EOF(self, line):
+        """Exits the interpreter on EOF"""
+        print()
+        return True
 
     def emptyline(self):
         """Does nothing on empty input"""
         pass
+
+    def help_quit(self):
+        """Help information for quit"""
+        print('Exits the program. Usage: quit')
     
-    def do_EOF(self, line):
-        return True
+    def help_EOF(self):
+        """Help information for EOF"""
+        print('Exits interpreter using EOF (Ctrl+D). Usage: EOF')
     
 
 if __name__ == "__main__":
