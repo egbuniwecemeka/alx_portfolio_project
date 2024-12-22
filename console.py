b2fd2911199c13aa12326f667ddd5ad27b74844e
egbuniwecemeka <egbuniwecemeka.com>
2024-12-23 00:06:22 +0100

    create command for Console 0.1 added

--- console.py
+++ console.py
@@ -1,9 +1,26 @@
 import cmd
+from models import storage
+from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
     """Command interpreter class"""
 
     prompt = '(hbnb)'
+
+    def do_create(self, line):
+        """Creates an instance of BaseModel and saves it"""
+        if not line.strip():
+            print('** class name missing **')
+            return
+        
+        class_name = line.strip()
+        try:
+            cls = eval(class_name)
+            new_model = cls()
+            new_model.save()
+            print(new_model.id)
+        except NameError:
+            print("** class doesn't exist **")
 
     def do_quit(self, line):
         """Exits the program"""
