2a99e41cb14d65e2c6714aeb6e289e11a46f1cd1
egbuniwecemeka <egbuniwecemeka.com>
2024-12-22 18:55:15 +0100

    Console 0.0.1

---
+++ console.py
@@ -0,0 +1,13 @@
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """Command interpreter class"""
+    def do_greet(self, line):
+        return 'Hello'
+    
+    def do_EOF(self, line):
+        return True
+    
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
