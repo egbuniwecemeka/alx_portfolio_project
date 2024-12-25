c21b4e97afca0a33877742560431f18b00a7991a
egbuniwecemeka <egbuniwecemeka.com>
2024-12-25 03:55:52 +0100

    First User

--- test_save_reload_user.py
+++ test_save_reload_user.py
@@ -9,12 +9,22 @@ for obj_id in all_objs.keys():
     obj = all_objs[obj_id]
     print(obj)
 
-print("-- Create a new user --")
+print("-- Create a new User --")
 
 my_user = User()
 my_user.first_name = "Emeka"
 my_user.last_name = "Egbuniwe"
 my_user.email = "egbuniwecemeka@gmail.com"
 my_user.password = "welcome1234"
+my_user.save()
+print(my_user)
+
+print("-- Create a new User 2 --")
+
+my_user = User()
+my_user.first_name = "Chidewu"
+my_user.last_name = "Blessing"
+my_user.email = "blessing1234@gmail.com"
+my_user.password = "welcome12345"
 my_user.save()
 print(my_user)
