6002ec4bcf3fd4c36a6103b97e6bef0905bccc72
egbuniwecemeka <egbuniwecemeka.com>
2024-12-26 23:04:01 +0100

    DBStorage - States and Cities

--- models/base_model.py
+++ models/base_model.py
@@ -3,8 +3,13 @@
 
 # Imports
 from uuid import uuid4
+import models
 from datetime import datetime
 from sqlalchemy import Column, DateTime, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
 
 class BaseModel:
     """BaseModel(Parent) class that defines attribute/methods for other classes"""
@@ -27,21 +32,21 @@ class BaseModel:
         if kwargs:
             for key, value in kwargs.items():
                 # Convert string timestamp format to datetime objects
-                if key == 'created_at' or key == 'updated_at':
+                if key in ['created_at', 'updated_at'] and isinstance(value, str):
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 # Ignores __class__ key if present & sets instance attribute
                 if key != '__class__':
                     setattr(self, key, value)
 
-                # Handle missing attributes
-                if "id" not in kwargs:
-                    self.id = str(uuid4())
-                if "created_at" not in kwargs:
-                    self.created_at = datetime.now()
-                if "updated_at" not in kwargs:
-                    self.updated_at = datetime.now()
-        # If kwarg is empty
+            # Handle missing attributes after processing kwargs
+            if "id" not in kwargs:
+                self.id = str(uuid4())
+            if "created_at" not in kwargs:
+                self.created_at = datetime.now()
+            if "updated_at" not in kwargs:
+                self.updated_at = datetime.now()
         else:
+            # Default initialization if kwarg is empty
             self.id = str(uuid4())
             self.created_at = self.updated_at = datetime.now()
 
@@ -51,17 +56,20 @@ class BaseModel:
     
     def save(self):
         """Updates current datetime of update_at attribute"""
-        from models import storage
         self.updated_at = datetime.now()
-        storage.new(self)
-        storage.save()
+        models.storage.new(self)
+        models.storage.save()
     
     def to_dict(self):
         """returns a dictionary containing key/value of __dict__ instance"""
-        dictionary = {}
+        dictionary = {key: value for key, value in self.__dict__.items() if key != '_sa_instance_state'}
         dictionary.update(self.__dict__)
         dictionary.update({'__class__': self.__class__.__name__})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
 
         return dictionary
+    
+    def delete(self):
+        """Deletes the current instance from storage"""
+        models.storage.delete(self)
