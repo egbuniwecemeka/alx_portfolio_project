1e7591406672fa41c1ee4a78ebdea6e41ecde6e5
egbuniwecemeka <egbuniwecemeka.com>
2024-12-27 02:34:12 +0100

    Console improvements

--- models/state.py
+++ models/state.py
@@ -8,24 +8,4 @@ from sqlalchemy.orm import relationship
 
 class State(BaseModel, Base):
     """State class inheriting from BaseModel"""
-
-    # Public class attribute
-    __tablename__ = 'states'
-
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", backref='state',
-                              cascade='all delete-orphan')
-    else:
-        name = ""   
-    
-    @property
-    def cities(self):
-        """Returns the list of City instances
-            where state_id equals current id
-        """
-        cities = list()
-        for _id, city in models.storage.all(City).items():
-            if city.state_id == self.id:
-                cities.append(city)
-        return cities
+    name = ""
