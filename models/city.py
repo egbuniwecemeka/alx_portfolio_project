ff614338d7bec8f38df85cc58697ff77d7546b80
egbuniwecemeka <egbuniwecemeka.com>
2024-12-27 04:38:35 +0100

    DBStorage - States and Cities

--- models/city.py
+++ models/city.py
@@ -1,6 +1,8 @@
 from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """City class inheriting from BaseModel"""
-    state_id = ""
-    name = ""
+    __tablename__ = "cities"
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('state.id'), nullable=False)
--- models/state.py
+++ models/state.py
@@ -1,11 +1,27 @@
 from os import getenv
-import models
+from models import storage
 from models.base_model import BaseModel, Base
 from models.city import City
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """State class inheriting from BaseModel"""
-    name = ""
+
+    __tablename__ = "states"
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", cascade='all delete-orphan',
+                              backref="state")
+    else:
+        name = ""
+
+    @property
+    def cities(self):
+        """Returns the list of City instances
+            where state_id euals to the current State.id
+        """
+        all_cities = storage.all(City)
+        return [city for city in all_cities.values() if city.state_id == self.id]
