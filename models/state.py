014cb1de1baddc9a78a42bb0e78c5119fa85966c
egbuniwecemeka <egbuniwecemeka.com>
2024-12-28 09:38:18 +0100

    DBStorage - States and Cities

--- models/state.py
+++ models/state.py
@@ -13,7 +13,7 @@ class State(BaseModel, Base):
 
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         name = Column(String(128), nullable=False)
-        cities = relationship("City", cascade='all delete-orphan',
+        cities = relationship("City", cascade=['all', 'delete-orphan'],
                               backref="state")
     else:
         name = ""
