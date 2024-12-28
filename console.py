9046dee9667fbe10da2918d4f54368e92364116b
egbuniwecemeka <egbuniwecemeka.com>
2024-12-28 14:41:11 +0100

    DBStorage - States and Cities

--- console.py
+++ console.py
@@ -4,6 +4,7 @@ from models.base_model import BaseModel
 from models.user import User
 from models.state import State
 from models.place import Place
+from models.city import City
 
 class HBNBCommand(cmd.Cmd):
     """Command interpreter class"""
