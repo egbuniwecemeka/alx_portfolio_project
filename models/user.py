859307e8cb7f791cf09bbcfe46c7d5daefc758a7
egbuniwecemeka <egbuniwecemeka.com>
2024-12-28 15:31:57 +0100

    DBStorage - User

--- models/user.py
+++ models/user.py
@@ -1,11 +1,13 @@
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """User class that inherits from BaseModel
         It represents a user with email, password, first_name and last_name
     """
+    __tablename__ = 'users'
     # Public attribute
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
