dc6e133767b41b23d379207e8cd9857fadda4efc
egbuniwecemeka <egbuniwecemeka.com>
2025-01-02 01:31:02 +0100

    Improve engines

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -7,18 +7,17 @@ from models.state import State
 app = Flask(__name__)
 
 
+@app.teardown_appcontext
+def remove_session(exception=None):
+    """Closes the current session"""
+    storage.close()
+
+
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """"""
-    data = storage.all(State)
-    sorted_states = sorted(data.values(), key=lambda x: x.name)
-    return render_template('7-states_list.html', states=sorted_states)
-
-
-@app.teardown_appcontext
-def remove_session(exception=None):
-    """Removes the current session"""
-    storage.close()
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states)
 
 
 if __name__ == "__main__":
