de66eeb69a80d332ecd0ef16782324b96e01b7b0
egbuniwecemeka <egbuniwecemeka.com>
2025-01-04 23:44:28 +0100

    Some stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
-""""""
+"""Index, routes and methods"""
 
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status', methods=['GET'])
@@ -15,6 +21,7 @@ def status():
 @app_views.route('/stats', methods=['GET'])
 def stats():
     """Retrieves the number of each objects by type"""
-    object_types = ["Amenity", "City", "Place", "Review", "State", "User"]
-    stats_data = {obj: storage.count(obj) for obj in object_types}
+    classes = [Amenity, City, Place, Review, State, User]
+    names = ['amenities', 'cities', 'places', 'states', 'users']
+    stats_data = {names[obj]: storage.count(classes[obj]) for obj in range(len(classes))}
     return jsonify(stats_data)
