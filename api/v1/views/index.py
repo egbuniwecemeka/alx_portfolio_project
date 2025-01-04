b422d5bc8e2520d3e1776c96c2d0f26dae8631ea
egbuniwecemeka <egbuniwecemeka.com>
2025-01-04 23:18:15 +0100

    Status of your API

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -14,5 +14,7 @@ def status():
 
 @app_views.route('/stats', methods=['GET'])
 def stats():
-    """Retrieves the number of objects by type"""
-    return storage.count()
+    """Retrieves the number of each objects by type"""
+    object_types = ["Amenity", "City", "Place", "Review", "State", "User"]
+    stats_data = {obj: storage.count(obj) for obj in object_types}
+    return stats_data
